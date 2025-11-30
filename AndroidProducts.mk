ee05ef3d60d3198f3ca8ddf7ad75bcf38b317677
Haitian001 <110573928+Haitian001@users.noreply.github.com>
2025-11-30 17:16:27 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_pearl.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_zircon.mk
 
 COMMON_LUNCH_CHOICES := \
     twrp_zircon-user \
